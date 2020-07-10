b46ecf1e72ba9f1706862aa6e90ac6eab1037a87
imtiyazalikhan <65025085+imtiyazalikhan@users.noreply.github.com>
2020-07-10 06:23:26 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pj-34
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,20 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+function setup(){
+createCanvas(800,400);
+}
+function draw(){
+background("white")
+}
+function mousePress(){
+  strokeWeight(4);
+  
+}
+
+function mouseRelease(){
+  strokeWeight();
+}
